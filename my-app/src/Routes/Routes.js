a45bb0cb00376197a6abe1f8c1f7c137dde21cd4
nadarajah Lucshin <nadarajahlucshini@gmail.com>
2024-07-07 17:31:44 +0530

    change

--- my-app/src/Routes/Routes.js
+++ my-app/src/Routes/Routes.js
@@ -2,11 +2,18 @@ import { lazy } from "react";
 
 // Lazy loading components
 const Home = lazy(() => import("../Pages/HomePage"));
+const About = lazy(() => import("../Pages/AboutMe"));
+const Contact = lazy(() => import("../Pages/Contactme"));
+const Project = lazy(() => import("../Pages/MyProject"));
 
 
 
 const MainRoute = [
     { path: "/", element: <Home/> },
+    { path: "/project", element: <Project/> },
+    { path: "/aboutme", element: <About/> },
+    { path: "/contact", element: <Contact/> },
+
     
 ];
 
